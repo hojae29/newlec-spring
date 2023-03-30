102de98465d3856f4a91072642f82f87b0cb7d2e
hojae29 <gkqthd6386@naver.com>
2023-03-30 18:00:28 +0900

    스프링 프레임워크 강의 14강 - @Autowired의 위치와 required 옵션

--- src/spring/di/ui/GridExamConsole.java
+++ src/spring/di/ui/GridExamConsole.java
@@ -6,13 +6,18 @@ import spring.di.entity.Exam;
 
 public class GridExamConsole implements ExamConsole {
 
+	// 필드에 @Autowired 붙일시 기본생성자를 통해 DI
+	// required 옵션을 false로 설정하면 등록된 빈이 없어도 오류가 발생하지 않는다.
+	@Autowired(required = false)
+	@Qualifier("exam2")
 	private Exam exam;
 	
 	public GridExamConsole() {
-	
+		System.out.println("constructor");
 	}
 
 	public GridExamConsole(Exam exam) {
+		System.out.println("overloaded constructor");
 		this.exam = exam;
 	}
 	
@@ -25,10 +30,9 @@ public class GridExamConsole implements ExamConsole {
 		System.out.println("└─────────┴─────────┘");
 	}
 
-	@Autowired
-	@Qualifier("exam2")
 	@Override
 	public void setExam(Exam exam) {
+		System.out.println("setter injection");
 		this.exam = exam;
 	}
 }
