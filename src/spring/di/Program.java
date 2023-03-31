6f63f87bb4fae456bb7fe1cafc73311f0def4d31
hojae29 <gkqthd6386@naver.com>
2023-03-31 10:06:11 +0900

    스프링 프레임워크 강의 17강 - XML Configuration을 Java Configuration으로 변경하기

--- src/spring/di/Program.java
+++ src/spring/di/Program.java
@@ -1,17 +1,16 @@
 package spring.di;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.di.ui.ExamConsole;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 
 		// console 안의 exam 객체는 @Autowired 어노테이션이 붙어있기 때문에 컴포넌트 스캔을 하면서 자동 주입 (DI)
-		// Exam 객체도 @Component 어노테이션이 붙어있어야 주입이 되고, xml 설정에 component-scan의 base-package를 설정해줘야한다.
 		ExamConsole console = (ExamConsole) context.getBean("console");
 		console.print();
 	}
